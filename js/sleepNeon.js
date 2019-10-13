c15465643ad62e0a64ba3996ff15d4d72726bec7
OPTIMUS <optimusprimeaoe.transformers@gmail.com>
2019-10-13 21:25:07 +0900

    Add files via upload

---
+++ js/sleepNeon.js
@@ -0,0 +1,1 @@
+﻿function startNEON(){var a=document.createElement("link");a.rel="stylesheet";a.href="css/style.css";a.href="css/style2.css";document.head.appendChild(a)}setTimeout("startNEON()",7500);
